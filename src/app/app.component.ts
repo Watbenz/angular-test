5417eb5289875fe32515473758c8f495ca4b2bad
watbenz <watsith@hotmail.com>
2020-02-25 13:34:49 +0700

    'week1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { TodoService } from './sevices/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project1';
-  loading = true;
+  title = 'World!';
+  isLoading = true;
+
+  amount = 123456.789123456789;
+  today = new Date();
+  todoList: number[] = [];
+
+  email: string;
+
+  imageUrl = 'https://placeimg.com/300/300/animals';
+
+  isActive = true;
+
+  user = {
+    name: 'Utsumaki Naruto', 
+    age: 31,
+    isNinja: true
+  }
+
+  constructor(
+    private todoService: TodoService
+  ) {
+    this.todoList = this.todoService.getTodoList();
+  }
+
+  onClickOkButton() {
+    console.log('You have clicked OK!');
+    this.email = "";
+  }
+
+  search(email: string) {
+    console.log('search', email);
+
+  }
 }
